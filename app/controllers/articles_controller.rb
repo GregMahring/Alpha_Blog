e22100c5222828981ba8d05ea06baa31cfebf31c
Greg Mahring <greg.mahring@leandogsoftware.com>
2017-03-17 08:51:51 -0400

    Styled Show and List pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
+      render 'new'
     end
   end
 
