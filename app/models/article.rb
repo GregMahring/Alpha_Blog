dce1b2682c3509269d6c3666e08a08912653c8b8
Greg Mahring <greg.mahring@leandogsoftware.com>
2017-03-31 08:44:39 -0400

    Fixed test for Rails 5

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 
   belongs_to :user
 
-  has_many :article_categories
+  has_many :categories, class_name: 'ArticleCategory'
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
